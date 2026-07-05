f230019753839fe2fc250c331d0f8faf26006e0b
reenat <reenat056@yandex.ru>
2026-07-05 18:17:41 +0300

    14. SessionController skeleton

---
+++ lib/features/session/application/session_controller.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../domain/session_mode.dart';
+import '../domain/session_status.dart';
+import 'session_state.dart';
+
+class SessionController extends Notifier<SessionState> {
+  @override
+  SessionState build() {
+    return const SessionState(
+      mode: SessionMode.idle,
+      status: SessionStatus.stopped,
+      startedAt: null,
+      endsAt: null,
+      pausedRemaining: null,
+      completedSessionsToday: 0,
+      totalCompletedSessions: 0,
+      soundEnabled: true,
+    );
+  }
+}
+
+final sessionControllerProvider =
+    NotifierProvider<SessionController, SessionState>(SessionController.new);
